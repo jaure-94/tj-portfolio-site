ca64ab9b648034481ad1632a4de493a1fb5381ff
Tawanda Jaure <tawandajaujau@gmail.com>
2022-10-18 05:02:47 +0200

    outline navbar component structure
    > add logo to navbar
    > call .map (es6) on array of menu items
    > invoke iife inside .map that displays menu item with anchor
      tag

--- react-frontend/src/components/Navbar/Navbar.jsx
+++ react-frontend/src/components/Navbar/Navbar.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 
 import './Navbar.scss'
+import { images } from '../../constants'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav>
+      <div>
+        <img src={images.logo} alt="logo" />
+      </div>
+      <ul>
+        {['home', 'about', 'contact', 'work', 'skills'].map((menuItem) => (
+          <li key={`link-${menuItem}`}>
+            <div />
+            <a href={`#${menuItem}`}>{menuItem}</a>
+          </li>
+        ))}
+      </ul>
+    </nav>
   )
 }
 
